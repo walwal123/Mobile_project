fda07ce77b88e1a67b1d42c1d9ac45cd81d31f5f
seungwon <herbysw@naver.com>
2023-12-02 02:11:02 +0900

    식사 입력하기 페이지 추가

---
+++ app/src/main/java/com/example/mobilesf/Food_input.java
@@ -0,0 +1,14 @@
+package com.example.mobilesf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Food_input extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_input);
+    }
+}
