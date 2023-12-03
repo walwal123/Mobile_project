1b5e9f9a6471372833d106a8e7487c8cc0b40b83
seungwon <herbysw@naver.com>
2023-12-04 00:10:07 +0900

    자세히 보기 추가, 사진 불러오기(임시)

---
+++ app/src/main/java/com/example/mobilesf/DetailActivity.java
@@ -0,0 +1,109 @@
+package com.example.mobilesf;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.Nullable;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class DetailActivity extends AppCompatActivity {
+    private static final int PERMISSION_REQUEST_CODE = 1;
+    private static final int OPEN_DOCUMENT_REQUEST_CODE = 2;
+    private ImageView imageView;
+    private Uri imageUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        TextView detailTextView = findViewById(R.id.textView5);
+        int position = 0;
+
+        Intent intent = getIntent();
+        if (intent != null && intent.hasExtra("position")) {
+            position = intent.getIntExtra("position", -1);
+            position += 1;
+            if (position != -1) {
+                // position을 사용하여 원하는 동작 수행
+                detailTextView.setText("Clicked position: " + position);
+            }
+        }
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            // 권한이 있으면 이미지 로딩
+            loadImage();
+        } else {
+            // 권한이 없으면 권한 요청
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+        }
+
+        //주어진 position값으로 쿼리 검색
+        String[] columns = new String[]{"_id", "restaurant", "picture", "food_name", "taste", "date", "cost"};
+        String selection = "_id=?";
+        String[] selectionArgs = new String[]{String.valueOf(position)};
+        Cursor c = getContentResolver().query(MyContentProvider.CONTENT_URI, columns, selection, selectionArgs, null, null);
+        if (c != null) {
+            EditText editMultipleText = findViewById(R.id.editTextText6);
+            editMultipleText.setText("");
+            while (c.moveToNext()) {
+                int id = c.getInt(0);
+                String rest = c.getString(1);
+                String pic = c.getString(2);
+                imageUri = Uri.parse(pic);
+                String name = c.getString(3);
+                String taste = c.getString(4);
+                String date = c.getString(5);
+                String cost = c.getString(6);
+                editMultipleText.append("id:" + id + "// 식당:" + rest + "// 사진주소:" + pic + "// 메뉴이름:" + name + "// 평가:" + taste + "// 날짜:" + date + "// 비용:" + cost);
+            }
+            c.close();
+        }
+
+        imageView = findViewById(R.id.imageView);
+        loadImage();
+    }
+
+
+    private void loadImage() {
+        if (imageUri != null) {
+            // 이미지 주소가 유효하다면 이미지를 표시
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("image/*");
+                startActivityForResult(intent, OPEN_DOCUMENT_REQUEST_CODE);
+            } else {
+                // 권한이 없으면 권한을 요청
+                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == OPEN_DOCUMENT_REQUEST_CODE && resultCode == RESULT_OK) {
+            if (data != null) {
+                Uri selectedImageUri = data.getData();
+                if (selectedImageUri != null) {
+                    imageView.setImageURI(selectedImageUri);
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/mobilesf/Food_output.java
+++ app/src/main/java/com/example/mobilesf/Food_output.java
@@ -1,10 +1,15 @@
 package com.example.mobilesf;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.app.ListActivity;
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -37,5 +42,15 @@ public class Food_output extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mealData);
         listView.setAdapter(adapter);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // 클릭한 아이템의 id를 상세 화면으로 전달
+                Intent intent = new Intent(Food_output.this, DetailActivity.class);
+                intent.putExtra("position", position);
+                startActivity(intent);
+            }
+        });
     }
+
 }
